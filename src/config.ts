c0b76d00434cedf7002073c559345d84a29e537b
liangkaze <2233217708az@gmail.com>
2026-04-08 23:42:28 +0800

    1

--- src/config.ts
+++ src/config.ts
@@ -26,7 +26,7 @@ const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UT
 export const siteConfig: SiteConfig = {
 	title: "liangkazeの博客",
 	subtitle: "",
-	siteURL: "https://liangkazesblog.edgeone-pages.com/", // 请替换为你的站点URL，以斜杠结尾
+	siteURL: "https://www.liangkaze.com/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2026-04-07", // 站点开始运行日期，用于站点统计组件计算运
 
 	timeZone: SITE_TIMEZONE,
@@ -57,9 +57,9 @@ export const siteConfig: SiteConfig = {
 		// 顶栏标题文本
 		text: "liangkazeの博客",
 		// 顶栏标题图标路径，默认使用 public/assets/home/home.webp
-		icon: "assets/home/home.webp",
+		icon: "assets/home/home.png",
 		// 网站Logo图片路径
-		logo: "assets/home/default-logo.webp",
+		logo: "assets/home/default-logo.png",
 	},
 
 	// 页面自动缩放配置
@@ -164,13 +164,7 @@ export const siteConfig: SiteConfig = {
 			enable: true, // 在主页显示自定义文本
 			title: "Wellcome to my blog!!!", // 主页横幅主标题
 
-			subtitle: [
-				"特別なことはないけど、君がいると十分です",
-				"今でもあなたは私の光",
-				"君ってさ、知らないうちに私の毎日になってたよ",
-				"君と話すと、なんか毎日がちょっと楽しくなるんだ",
-				"今日はなんでもない日。でも、ちょっとだけいい日",
-			],
+			subtitle: ["保持热爱，奔赴山海。"],
 			typewriter: {
 				enable: true, // 启用副标题打字机效果
 
